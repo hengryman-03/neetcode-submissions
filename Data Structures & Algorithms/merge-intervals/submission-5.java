bf67e0aaf36617e2270ae8ce61000365078fdfb9
Tiong Heng Rui <128883646+hengryman-03@users.noreply.github.com>
2026-09-05 15:15:12 +0800

    Add: merge-intervals - submission-5

---
+++ Data Structures & Algorithms/merge-intervals/submission-5.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        // Fix: Use Integer instead of Integers
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        int n = intervals.length;
+        int[][] res = new int[n][2];
+        res[0] = intervals[0];
+        
+        int e = 0;
+        // Fix: Start loop at 1 since res[0] is already populated
+        for(int i = 1; i < n; i++){
+            if (intervals[i][0] <= res[e][1]){
+                res[e][1] = Math.max(res[e][1], intervals[i][1]);
+            } else{
+                e++;
+                res[e] = intervals[i];
+            }
+        }
+        return Arrays.copyOfRange(res, 0, e + 1);
+    }
+}
